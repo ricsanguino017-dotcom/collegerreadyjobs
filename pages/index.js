b47fdfb8b9f3589b54911adc8d177fe4d4cf037b
Ricardo Sanguino <ricardosanguino@Ricardos-MacBook-Air.local>
2026-05-28 18:33:48 -0400

    Fix: read HTML from pages folder

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ export async function getServerSideProps(context) {
   const { res } = context
   const fs = require('fs')
   const path = require('path')
-  const html = fs.readFileSync(path.join(process.cwd(), 'public', 'index.html'), 'utf8')
-  res.setHeader('Content-Type', 'text/html')
+  const html = fs.readFileSync(path.join(process.cwd(), 'pages', 'index.html'), 'utf8')
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
   res.write(html)
   res.end()
   return { props: {} }
