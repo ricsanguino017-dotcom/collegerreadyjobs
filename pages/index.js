f3e789c684524cc8d1387e216eeb25c0f4bfe6f1
Ricardo Sanguino <ricardosanguino@Ricardos-MacBook-Air.local>
2026-05-28 18:25:08 -0400

    Fix: serve HTML directly

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() { return null }
+export async function getServerSideProps(context) {
+  const { res } = context
+  const fs = require('fs')
+  const path = require('path')
+  const html = fs.readFileSync(path.join(process.cwd(), 'public', 'index.html'), 'utf8')
+  res.setHeader('Content-Type', 'text/html')
+  res.write(html)
+  res.end()
+  return { props: {} }
+}
